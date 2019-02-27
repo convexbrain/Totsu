27fc2db30078fbf3f391a35e7038f68b7188df72
convexbrain <convexbrain@gmail.com>
2019-02-27 16:43:00 +0900

    updated: refactoring, ploting and README

--- examples/ctrl_toyprob/README.md
+++ examples/ctrl_toyprob/README.md
@@ -1,6 +1,8 @@
 # Example: ctrl_toyprob
 
-* 
+* Simple path planning or trajectory planning
+* Given 4 target positions including start and end points
+* Minimize velocity with acceleration limits
 
 ## Running this Example
 
@@ -9,9 +11,16 @@ $ cargo run --release
 $ python plot.py
 ```
 
-![](plot.png)
+![](plot1.png)
 
-* 
+* Trajectory at every time interval
+* Red star points: the target positions
+
+![](plot2.png)
+
+* Time profile of velocity and acceleration magnitude
+* Acceleration is kept under limit of 90
+* Uniform linear motion is preferred under the constraints of acceleration and target positions
 
 ## Formulation
 
--- examples/ctrl_toyprob/plot.py
+++ examples/ctrl_toyprob/plot.py
@@ -5,21 +5,35 @@ dat = np.loadtxt("dat_point")
 #print(dat.shape)
 #print(dat)
 #print(dat[:,0])
+dat_t = np.loadtxt("dat_target")
 
-plt.plot(dat[:, 0], dat[:, 1], marker='o')
+if True:
+    plt.plot(dat_t[:, 0], dat_t[:, 1], 'r*', markersize=20, markerfacecolor='w')
+    plt.plot(dat[:, 1], dat[:, 2], marker='o')
+    plt.xlabel('x0')
+    plt.ylabel('x1')
+    plt.show()
 
-#plt.xlim(0, 1)
-#plt.ylim(0, 1)
-plt.show()
+#####
 
+dt = dat[1, 0] - dat[0, 0]
 t = dat.shape[0]
-vx = dat[1: t, 0] - dat[0: t - 1, 0]
-vy = dat[1: t, 1] - dat[0: t - 1, 1]
-v = np.sqrt(vx * vx + vy * vy)
-ax = dat[2: t, 0] - 2 * dat[1: t - 1, 0] + dat[0: t - 2, 0]
-ay = dat[2: t, 1] - 2 * dat[1: t - 1, 1] + dat[0: t - 2, 1]
-a = np.sqrt(ax * ax + ay * ay)
 
-plt.plot(v, marker='o')
-plt.plot(a, marker='o')
-plt.show()
+vx = dat[1: t, 1] - dat[0: t - 1, 1]
+vy = dat[1: t, 2] - dat[0: t - 1, 2]
+v = np.sqrt(vx * vx + vy * vy) / dt
+
+ax = dat[2: t, 1] - 2 * dat[1: t - 1, 1] + dat[0: t - 2, 1]
+ay = dat[2: t, 2] - 2 * dat[1: t - 1, 2] + dat[0: t - 2, 2]
+a = np.sqrt(ax * ax + ay * ay) / dt / dt
+
+if True:
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(dat[0: t - 1, 0], v, marker='.')
+    ax[1].plot(dat[0: t - 2, 0], a, marker='.')
+    ax[1].set_xlim(0, 1)
+    ax[0].set_xlim(0, 1)
+    ax[1].set_xlabel('time')
+    ax[0].set_ylabel('velocity mag.')
+    ax[1].set_ylabel('acceleration mag.')
+    plt.show()
