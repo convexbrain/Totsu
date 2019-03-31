22c56233b6a093aedf8bd9652216e1960ba5621a
convexbrain <convexbrain@gmail.com>
2019-03-31 18:51:19 +0900

    updated: rgb

--- examples/imgproc/src/main.rs
+++ examples/imgproc/src/main.rs
@@ -4,51 +4,64 @@ use totsu::mat::XOR64;
 
 #[macro_use] extern crate itertools;
 
+fn clamp(v: FP) -> u8 {
+    let v = if v < 0. {0.} else {v};
+    let v = if v > 1. {1.} else {v};
+    (v * 255.) as u8
+}
+
 /// main
 fn main() -> std::io::Result<()> {
     let mut rnd = XOR64::init();
 
     let sz = 8;
+    let ch = 3;
 
-    let n: usize = sz * sz + (sz - 2) * (sz - 2);
+    let n: usize = sz * sz * ch + (sz - 2) * (sz - 2);
     let m: usize = (sz - 2) * (sz - 2);
-    let p: usize = sz * 4 - 4;
+    let p: usize = (sz * 4 - 4) * ch;
 
     let mut vec_f = Mat::new_vec(n);
-    vec_f.rows_mut(sz * sz .. n).assign_all(1.);
+    vec_f.rows_mut(sz * sz * ch .. n).assign_all(1.);
 
-    let mut mat_g = vec![Mat::new(1, n); m];
-    let vec_h = vec![Mat::new_vec(1); m];
+    let mut mat_g = vec![Mat::new(ch, n); m];
+    let vec_h = vec![Mat::new_vec(ch); m];
     let mut vec_c = vec![Mat::new_vec(n); m];
     let scl_d = vec![0. as FP; m];
     for (x, y) in iproduct!(0 .. sz - 2, 0 .. sz - 2) {
         let i = x + y * (sz - 2);
-        mat_g[i][(0, (x + 0) + (y + 1) * sz)] = 2.;
-        mat_g[i][(0, (x + 2) + (y + 1) * sz)] = 2.;
-        mat_g[i][(0, (x + 1) + (y + 0) * sz)] = 2.;
-        mat_g[i][(0, (x + 1) + (y + 2) * sz)] = 2.;
-        mat_g[i][(0, (x + 1) + (y + 1) * sz)] = -4.;
-        mat_g[i][(0, (x + 0) + (y + 0) * sz)] = -1.;
-        mat_g[i][(0, (x + 2) + (y + 0) * sz)] = -1.;
-        mat_g[i][(0, (x + 0) + (y + 2) * sz)] = -1.;
-        mat_g[i][(0, (x + 2) + (y + 2) * sz)] = -1.;
-        vec_c[i][(sz * sz + i, 0)] = 1.;
+        for c in 0 .. ch {
+            let co = c * sz * sz;
+            mat_g[i][(c, (x + 0) + (y + 1) * sz + co)] = 2.;
+            mat_g[i][(c, (x + 2) + (y + 1) * sz + co)] = 2.;
+            mat_g[i][(c, (x + 1) + (y + 0) * sz + co)] = 2.;
+            mat_g[i][(c, (x + 1) + (y + 2) * sz + co)] = 2.;
+            mat_g[i][(c, (x + 1) + (y + 1) * sz + co)] = -4.;
+            mat_g[i][(c, (x + 0) + (y + 0) * sz + co)] = -1.;
+            mat_g[i][(c, (x + 2) + (y + 0) * sz + co)] = -1.;
+            mat_g[i][(c, (x + 0) + (y + 2) * sz + co)] = -1.;
+            mat_g[i][(c, (x + 2) + (y + 2) * sz + co)] = -1.;
+        }
+        vec_c[i][(sz * sz * ch + i, 0)] = 1.;
     }
 
     let mut mat_a = Mat::new(p, n);
-    let vec_b = Mat::new_vec(p).set_by(|_, _| rnd.next());
+    let vec_b = Mat::new_vec(p).set_by(|_, _| rnd.next() * 0.5 + 0.25);
     let mut ip = 0;
-    for x in 0 .. sz {
-        mat_a[(ip, x)] = 1.;
-        ip += 1;
-        mat_a[(ip, x + (sz - 1) * sz)] = 1.;
-        ip += 1;
-    }
-    for y in 1 .. sz - 1 {
-        mat_a[(ip, y * sz)] = 1.;
-        ip += 1;
-        mat_a[(ip, (sz - 1) + y * sz)] = 1.;
-        ip += 1;
+    for c in 0 .. ch {
+        let co = c * sz * sz;
+        for x in 0 .. sz {
+            mat_a[(ip, x + co)] = 1.;
+            ip += 1;
+            mat_a[(ip, x + (sz - 1) * sz + co)] = 1.;
+            ip += 1;
+        }
+        for y in 1 .. sz - 1 {
+            mat_a[(ip, y * sz + co)] = 1.;
+            ip += 1;
+            mat_a[(ip, (sz - 1) + y * sz + co)] = 1.;
+            ip += 1;
+        }
     }
 
     //let param = PDIPMParam::default();
@@ -63,11 +76,12 @@ fn main() -> std::io::Result<()> {
 
     let mut img = image::ImageBuffer::new(sz as u32, sz as u32);
     for (x, y, p) in img.enumerate_pixels_mut() {
-        let v = rslt[(x as usize + y as usize * sz, 0)];
-        let v = if v < 0. {0.} else {v};
-        let v = if v > 1. {1.} else {v};
-        let g = (v * 255.) as u8;
-        *p = image::Rgb([g, g, g]);
+        let xx = x as usize;
+        let yy = y as usize;
+        let r = clamp(rslt[(xx + yy * sz, 0)]);
+        let g = clamp(rslt[(xx + yy * sz + sz * sz, 0)]);
+        let b = clamp(rslt[(xx + yy * sz + 2 * sz * sz, 0)]);
+        *p = image::Rgb([r, g, b]);
     }
     img.save("img.bmp")?;
 
