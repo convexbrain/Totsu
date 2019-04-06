d3cc1e55702b21953734695f529a2b87116bba3b
convexbrain <convexbrain@gmail.com>
2019-04-07 00:02:22 +0900

    updated: svd criteria

--- examples/imgproc/src/main.rs
+++ examples/imgproc/src/main.rs
@@ -17,8 +17,9 @@ fn main() -> std::io::Result<()> {
     let sz = 8;
     let ch = 3;
 
-    let n: usize = sz * sz * ch + (sz - 2) * (sz - 2);
-    let m: usize = (sz - 2) * (sz - 2);
+    let n: usize = sz * sz * ch + sz * sz;
+    let m: usize = sz * sz;
+    //let p: usize = 4 * ch;
     let p: usize = (sz * 4 - 4) * ch;
 
     let mut vec_f = Mat::new_vec(n);
@@ -28,25 +29,32 @@ fn main() -> std::io::Result<()> {
     let vec_h = vec![Mat::new_vec(ch); m];
     let mut vec_c = vec![Mat::new_vec(n); m];
     let scl_d = vec![0. as FP; m];
-    for (x, y) in iproduct!(0 .. sz - 2, 0 .. sz - 2) {
-        let i = x + y * (sz - 2);
-        for c in 0 .. ch {
-            let co = c * sz * sz;
-            mat_g[i][(c, (x + 0) + (y + 1) * sz + co)] = 2.;
-            mat_g[i][(c, (x + 2) + (y + 1) * sz + co)] = 2.;
-            mat_g[i][(c, (x + 1) + (y + 0) * sz + co)] = 2.;
-            mat_g[i][(c, (x + 1) + (y + 2) * sz + co)] = 2.;
-            mat_g[i][(c, (x + 1) + (y + 1) * sz + co)] = -4.;
-            mat_g[i][(c, (x + 0) + (y + 0) * sz + co)] = -1.;
-            mat_g[i][(c, (x + 2) + (y + 0) * sz + co)] = -1.;
-            mat_g[i][(c, (x + 0) + (y + 2) * sz + co)] = -1.;
-            mat_g[i][(c, (x + 2) + (y + 2) * sz + co)] = -1.;
+    {
+        let mut i = 0;
+        for (x, y) in iproduct!(0 .. sz, 0 .. sz) {
+            for c in 0 .. ch {
+                let co = c * sz * sz;
+
+                let mut t = 0;
+                for (dx, dy) in iproduct!(-1_i32 ..= 1, -1_i32 ..= 1) {
+                    let xx = x as i32 + dx;
+                    let yy = y as i32 + dy;
+                    if (xx >= 0) && (xx < sz as i32) && (yy >= 0) && (yy < sz as i32) {
+                        t += 1;
+                        let xx = xx as usize;
+                        let yy = yy as usize;
+                        mat_g[i][(c, xx + yy * sz + co)] = -1.;
+                    }
+                }
+                mat_g[i][(c, x + y * sz + co)] = (t - 1) as FP;
+            }
+            vec_c[i][(sz * sz * ch + i, 0)] = 1.;
+            i += 1;
         }
-        vec_c[i][(sz * sz * ch + i, 0)] = 1.;
     }
 
     let mut mat_a = Mat::new(p, n);
-    let vec_b = Mat::new_vec(p).set_by(|_, _| rnd.next() * 0.5 + 0.25);
+    let vec_b = Mat::new_vec(p).set_by(|_, _| rnd.next());
     let mut ip = 0;
     for c in 0 .. ch {
         let co = c * sz * sz;
@@ -63,13 +71,29 @@ fn main() -> std::io::Result<()> {
             ip += 1;
         }
     }
+    /*
+    {
+        let mut i = 0;
+        for c in 0 .. ch {
+            let co = c * sz * sz;
+            mat_a[(i, co)] = 1.;
+            i += 1;
+            mat_a[(i, sz - 1 + co)] = 1.;
+            i += 1;
+            mat_a[(i, (sz - 1) * sz + co)] = 1.;
+            i += 1;
+            mat_a[(i, sz - 1 + (sz - 1) * sz + co)] = 1.;
+            i += 1;
+        }
+    }
+    */
 
     //let param = PDIPMParam::default();
     let param = PDIPMParam {
-        eps: 1e-3,
+        eps: 1. / 256.,
         .. Default::default()
     };
-    let rslt = PDIPM::new().solve_socp(&param, &mut std::io::sink(),
+    let rslt = PDIPM::new().solve_socp(&param, &mut std::io::stdout(),
                                        &vec_f,
                                        &mat_g, &vec_h, &vec_c, &scl_d,
                                        &mat_a, &vec_b).unwrap();
@@ -83,7 +107,7 @@ fn main() -> std::io::Result<()> {
         let b = clamp(rslt[(xx + yy * sz + 2 * sz * sz, 0)]);
         *p = image::Rgb([r, g, b]);
     }
-    img.save("img.bmp")?;
+    img.save("img.png")?;
 
     Ok(())
 }
--- solver_rust/src/matsvd.rs
+++ solver_rust/src/matsvd.rs
@@ -47,7 +47,7 @@ impl MatSVD
         let b = self.u.col(c2).norm_p2sq();
         let d = self.u.col(c1).prod(&self.u.col(c2));
 
-        if d * d <= TOL_CNV2 * a * b {
+        if (d * d <= TOL_CNV2 * a * b) || (d.abs() <= TOL_CNV2) {
             true
         }
         else {
