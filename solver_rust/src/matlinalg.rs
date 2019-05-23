6c15c41d05bf7021307f66dc9f6cffc41c883251
convexbrain <convexbrain@gmail.com>
2019-05-24 00:26:18 +0900

    updated: lsqr

--- solver_rust/src/matlinalg.rs
+++ solver_rust/src/matlinalg.rs
@@ -164,6 +164,102 @@ fn solve_lsmr<L: LinOp>(lop_a: &L, vec_b: MatSlice) -> Mat
     }
 }
 
+fn solve_lsqr<L: LinOp>(lop_a: &L, vec_b: MatSlice) -> Mat
+{
+    const ATOL: FP = FP_EPSILON;
+    const BTOL: FP = FP_EPSILON;
+    const CONLIM: FP = 1. / FP_EPSILON;
+    
+    let (m, n) = lop_a.size();
+    const ZERO: FP = 0.;
+
+    assert_eq!(vec_b.size(), (m, 1));
+    
+    // 1. Initialize
+    let (beta_1, mut u_1) = normalize(vec_b);
+    let (mut alpha_1, mut v_1) = normalize(lop_a.t_apply(&u_1).as_slice());
+    let mut w_1 = v_1.clone_sz();
+    let mut x_0 = Mat::new_vec(n);
+    let mut phi_b1 = beta_1;
+    let mut rho_b1 = alpha_1;
+    //
+    let mut norm_r_0 = beta_1;
+    let mut sqnorm_b_0 = ZERO;
+    let mut sqnorm_d_0 = ZERO;
+    let norm_b = beta_1;
+
+    if !(beta_1 > 0.) || !(alpha_1 > 0.) {
+        return x_0;
+    }
+
+    // 2. For i = 1, 2, 3 . . . , repeat steps 3–6.
+    loop {
+        // (indexing is shown as if i = 1)
+
+        // 3. Continue the bidiagonalization
+        let (beta_2, u_2) = normalize((lop_a.apply(&v_1) - alpha_1 * u_1).as_slice());
+        let (alpha_2, v_2) = normalize((lop_a.t_apply(&u_2) - beta_2 * v_1).as_slice());
+
+        // 4. Construct and apply next orthogonal transformation
+        let rho_1 = rho_b1.hypot(beta_2);
+        let c_1 = rho_b1 / rho_1;
+        let s_1 = beta_2 / rho_1;
+        let theta_2 = s_1 * alpha_2;
+        let rho_b2 = -c_1 * alpha_2;
+        let phi_1 = c_1 * phi_b1;
+        let phi_b2 = s_1 * phi_b1;
+
+        // 5. Update x, w
+        let x_1 = &x_0 + (phi_1 / rho_1) * &w_1;
+        let w_2 = &v_2 - (theta_2 / rho_1) * &w_1;
+
+        // (computing ||r_1||)
+        let norm_r_1 = norm_r_0 * s_1;
+
+        // (computing ||A'r_1||)
+        let norm_ar_1 = phi_b2 * alpha_2 * c_1.abs();
+
+        // (computing ||x_1||)
+        let norm_x_1 = x_1.norm_p2();
+
+        // (computing ||A||)
+        let sqnorm_b_1 = sqnorm_b_0 + alpha_1.powi(2) + beta_2.powi(2);
+        let norm_a = sqnorm_b_1.sqrt();
+
+        // (computing ||cond(A)||)
+        let sqnorm_delta_1 = w_1.norm_p2sq() / rho_1.powi(2);
+        let sqnorm_d_1 = sqnorm_d_0 + sqnorm_delta_1;
+        let cond_a = (sqnorm_b_1 * sqnorm_d_1).sqrt();
+
+        // (stopping criteria)
+        let s1 = norm_r_1 <= BTOL * norm_b + ATOL * norm_a * norm_x_1;
+        let s2 = norm_ar_1 <= ATOL * norm_a * norm_r_1;
+        let s3 = cond_a >= CONLIM;
+
+        if s1 || s2 || s3 {
+            return x_1;
+        }
+
+        // (update variables)
+        //
+        //beta_1 = beta_2;
+        u_1 = u_2;
+        alpha_1 = alpha_2;
+        v_1 = v_2;
+        //
+        rho_b1 = rho_b2;
+        phi_b1 = phi_b2;
+        //
+        x_0 = x_1;
+        w_1 = w_2;
+        //
+        norm_r_0 = norm_r_1;
+        //
+        sqnorm_b_0 = sqnorm_b_1;
+        sqnorm_d_0 = sqnorm_d_1;
+    }
+}
+
 /// Linear equation solver by LSMR
 /// 
 /// References
@@ -179,7 +275,7 @@ pub fn lin_solve<L: LinOp>(lop_a: &L, mat_b: &Mat) -> Mat
     for c in 0 .. xc {
         let vec_b = mat_b.col(c);
         let mut vec_x = mat_x.col_mut(c);
-        vec_x.assign(&solve_lsmr(lop_a, vec_b));
+        vec_x.assign(&solve_lsqr(lop_a, vec_b));
     }
 
     mat_x
