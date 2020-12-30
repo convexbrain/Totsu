0b89d60a96b02f756f28a4c1af6dda44a33c7ba9
convexbrain <convexbrain@gmail.com>
2020-12-30 17:27:06 +0900

    updated: LP works

--- solver_rust_conic/src/solver.rs
+++ solver_rust_conic/src/solver.rs
@@ -13,14 +13,6 @@ fn norm(x: &[f64]) -> f64
     unsafe { cblas::dnrm2(x.len() as i32, x, 1) }
 }
 
-fn normalize(x: &mut[f64])
-{
-    let n = norm(x);
-    if n > f64::MIN_POSITIVE {
-        scale(n.recip(), x);
-    }
-}
-
 fn inner_prod(x: &[f64], y: &[f64]) -> f64
 {
     assert_eq!(x.len(), y.len());
@@ -56,7 +48,11 @@ fn sp_norm<O: Operator>(op: &O) -> f64
     let mut lambda = 0.;
 
     loop {
-        normalize(&mut v);
+        let n = norm(&v);
+        if n > f64::MIN_POSITIVE {
+            scale(n.recip(), &mut v);
+        }
+
         op.op(1., &v, 0., &mut t);
         op.trans_op(1., &t, 0., &mut w);
 
@@ -168,14 +164,19 @@ where OC: Operator, OA: Operator, OB: Operator
         }
     }
 
-    fn b_norm(&self) -> f64
+    fn c(&self) -> &OC
     {
-        fr_norm(&self.b)
+        &self.c
     }
 
-    fn c_norm(&self) -> f64
+    fn a(&self) -> &OA
     {
-        fr_norm(&self.c)
+        &self.a
+    }
+
+    fn b(&self) -> &OB
+    {
+        &self.b
     }
 }
 
@@ -191,77 +192,122 @@ where OC: Operator, OA: Operator, OB: Operator
 
     fn op(&self, alpha: f64, x: &[f64], beta: f64, y: &mut[f64])
     {
-        let nm1 = self.n + self.m + 1;
+        let n = self.n;
+        let m = self.m;
+        
+        assert_eq!(y.len(), n + m + 1);
 
-        if x.len() == nm1 * 2 {
-            let (u_x, u) = x.split_at(self.n);
-            let (u_y, u) = u.split_at(self.m);
-            let (u_tau, v) = u.split_at(1);
-            let (v_r, v) = v.split_at(self.n);
-            let (v_s, v) = v.split_at(self.m);
-            let (v_kappa, _) = v.split_at(1);
-
-            let (w_n, w) = y.split_at_mut(self.n);
-            let (w_m, w) = w.split_at_mut(self.m);
-            let (w_1, _) = w.split_at_mut(1);
-
-            self.a.trans_op(alpha, u_y, beta, w_n);
-            self.c.op(alpha, u_tau, 1., w_n);
-            add(-alpha, v_r, w_n);
-
-            self.a.op(-alpha, u_x, beta, w_m);
-            self.b.op(alpha, u_tau, 1., w_m);
-            add(-alpha, v_s, w_m);
-
-            self.c.trans_op(-alpha, u_x, beta, w_1);
-            self.b.trans_op(-alpha, u_y, 1., w_1);
-            add(-alpha, v_kappa, w_1);
+        let (u_x, u) = x.split_at(n);
+        let (u_y, u) = u.split_at(m);
+        let (u_tau, v) = u.split_at(1);
+        let (v_r, v) = if v.len() == n + m + 1 {
+            v.split_at(n)
+        }
+        else if v.len() == m + 1 {
+            v.split_at(0)
         }
         else {
-            // TODO
             assert!(false);
+            v.split_at(0)
+        };
+        let (v_s, v) = v.split_at(m);
+        let (v_kappa, _) = v.split_at(1);
+
+        let (w_n, w) = y.split_at_mut(n);
+        let (w_m, w) = w.split_at_mut(m);
+        let (w_1, _) = w.split_at_mut(1);
+
+        self.a.trans_op(alpha, u_y, beta, w_n);
+        self.c.op(alpha, u_tau, 1., w_n);
+        if v_r.len() == w_n.len() {
+            add(-alpha, v_r, w_n);
         }
+
+        self.a.op(-alpha, u_x, beta, w_m);
+        self.b.op(alpha, u_tau, 1., w_m);
+        add(-alpha, v_s, w_m);
+
+        self.c.trans_op(-alpha, u_x, beta, w_1);
+        self.b.trans_op(-alpha, u_y, 1., w_1);
+        add(-alpha, v_kappa, w_1);
     }
 
     fn trans_op(&self, alpha: f64, x: &[f64], beta: f64, y: &mut[f64])
     {
-        let nm1 = self.n + self.m + 1;
+        let n = self.n;
+        let m = self.m;
+        
+        assert_eq!(x.len(), n + m + 1);
 
-        if y.len() == nm1 * 2 {
-            let (w_n, w) = x.split_at(self.n);
-            let (w_m, w) = w.split_at(self.m);
-            let (w_1, _) = w.split_at(1);
+        let (w_n, w) = x.split_at(n);
+        let (w_m, w) = w.split_at(m);
+        let (w_1, _) = w.split_at(1);
 
-            let (u_x, u) = y.split_at_mut(self.n);
-            let (u_y, u) = u.split_at_mut(self.m);
-            let (u_tau, v) = u.split_at_mut(1);
-            let (v_r, v) = v.split_at_mut(self.n);
-            let (v_s, v) = v.split_at_mut(self.m);
-            let (v_kappa, _) = v.split_at_mut(1);
-
-            self.a.trans_op(-alpha, w_m, beta, u_x);
-            self.c.op(-alpha, w_1, 1., u_x);
-
-            self.a.op(alpha, w_n, beta, u_y);
-            self.b.op(-alpha, w_1, 1., u_y);
-
-            self.c.trans_op(alpha, w_n, beta, u_tau);
-            self.b.trans_op(alpha, w_m, 1., u_tau);
-
-            scale(beta, v_r);
-            add(-alpha, w_n, v_r);
-
-            scale(beta, v_s);
-            add(-alpha, w_m, v_s);
-
-            scale(beta, v_kappa);
-            add(-alpha, w_1, v_kappa);
+        let (u_x, u) = y.split_at_mut(n);
+        let (u_y, u) = u.split_at_mut(m);
+        let (u_tau, v) = u.split_at_mut(1);
+        let (v_r, v) = if v.len() == n + m + 1 {
+            v.split_at_mut(n)
+        }
+        else if v.len() == m + 1 {
+            v.split_at_mut(0)
         }
         else {
-            // TODO
             assert!(false);
+            v.split_at_mut(0)
+        };
+        let (v_s, v) = v.split_at_mut(m);
+        let (v_kappa, _) = v.split_at_mut(1);
+
+        self.a.trans_op(-alpha, w_m, beta, u_x);
+        self.c.op(-alpha, w_1, 1., u_x);
+
+        self.a.op(alpha, w_n, beta, u_y);
+        self.b.op(-alpha, w_1, 1., u_y);
+
+        self.c.trans_op(alpha, w_n, beta, u_tau);
+        self.b.trans_op(alpha, w_m, 1., u_tau);
+
+        if v_r.len() == w_n.len() {
+            scale(beta, v_r);
+            add(-alpha, w_n, v_r);
         }
+
+        scale(beta, v_s);
+        add(-alpha, w_m, v_s);
+
+        scale(beta, v_kappa);
+        add(-alpha, w_1, v_kappa);
     }
+}
+
+fn proj_pos(x: &mut[f64])
+{
+    for e in x {
+        *e = e.max(0.);
+    }
+}
+
+fn proj_zero(x: &mut[f64])
+{
+    for e in x {
+        *e = 0.;
+    }
+}
+
+fn proj_psd(x: &mut[f64])
+{
+    panic!("not implemented");
+}
+
+fn proj_cone(x: &mut[f64])
+{
+    proj_pos(x);
+}
+
+fn proj_cone_conj(x: &mut[f64])
+{
+    proj_pos(x);
 }
 
 pub struct Solver;
@@ -275,7 +321,7 @@ impl Solver
 
     pub fn solve(&self)
     {
-        let max_iter = Some(3);
+        let max_iter = Some(5000);
 
         let n = 1;
         let m = 2;
@@ -306,22 +352,129 @@ impl Solver
         let eps_unbdd = 1e-6;
         let eps_infeas = 1e-6;
 
-        let b_norm = op_l.b_norm();
-        let c_norm = op_l.c_norm();
+        let b_norm = fr_norm(op_l.b());
+        let c_norm = fr_norm(op_l.c());
+
+        let mut x = vec![0.; n + (m + 1) * 2];
+        x[n + m] = 1.; // u_tau
+        x[n + m + 1 + m] = 1.; // v_kappa
+        let mut xx = x.clone();
+        let mut y = vec![0.; n + m + 1];
+
+        let mut p = vec![0.; m];
+        let mut d = vec![0.; n];
+        let mut one = vec![1.; 1];
 
         let mut i = 0;
         loop {
             println!("----- {}", i);
 
-            // TODO
+            op_l.trans_op(-tau, &y, 1.0, &mut x);
+
+            {
+                let (_, u) = x.split_at_mut(n);
+                let (u_y, u) = u.split_at_mut(m);
+                let (u_tau, v) = u.split_at_mut(1);
+                let (v_s, v) = v.split_at_mut(m);
+                let (v_kappa, _) = v.split_at_mut(1);
+
+                proj_cone_conj(u_y);
+                proj_pos(u_tau);
+                proj_cone(v_s);
+                proj_pos(v_kappa);
+            }
+
+            add(-2., &x, &mut xx);
+            op_l.op(-sigma, &xx, 1., &mut y);
+            copy(&x, &mut xx);
+
+            //println!("{:?}", x);
+            //println!("{:?}", y);
+
+            {
+                let (u_x, u) = x.split_at(n);
+                let (u_y, u) = u.split_at(m);
+                let (u_tau, v) = u.split_at(1);
+                let (v_s, _) = v.split_at(m);
+
+                let u_tau = u_tau[0];
+
+                if u_tau > eps_zero {
+                    one[0] = 1.;
+
+                    copy(v_s, &mut p);
+                    op_l.b().op(-1., &one, u_tau.recip(), &mut p);
+                    op_l.a().op(u_tau.recip(), u_x, 1., &mut p);
+    
+                    op_l.c().op(1., &one, 0., &mut d);
+                    op_l.a().trans_op(u_tau.recip(), u_y, 1., &mut d);
+    
+                    op_l.c().trans_op(u_tau.recip(), u_x, 0., &mut one);
+                    let g_x = one[0];
+    
+                    op_l.b().trans_op(u_tau.recip(), u_y, 0., &mut one);
+                    let g_y = one[0];
+    
+                    let g = g_x + g_y;
+
+                    let term_pri = norm(&p) <= eps_pri * (1. + b_norm);
+                    let term_dual = norm(&d) <= eps_dual * (1. + c_norm);
+                    let term_gap = g.abs() <= eps_gap * (1. + g_x.abs() + g_y.abs());
+    
+                    println!("{} {} {}", term_pri, term_dual, term_gap);
+
+                    if term_pri && term_dual && term_gap {
+                        println!("converged");
+                        break;
+                    }
+                }
+                else {
+                    copy(v_s, &mut p);
+                    op_l.a().op(1., u_x, 1., &mut p);
+
+                    op_l.a().trans_op(1., u_y, 0., &mut d);
+
+                    op_l.c().trans_op(-1., u_x, 0., &mut one);
+                    let m_cx = one[0];
+
+                    op_l.b().trans_op(-1., u_y, 0., &mut one);
+                    let m_by = one[0];
+        
+                    let term_unbdd = (m_cx > eps_zero) && (
+                        norm(&p) * c_norm <= eps_unbdd * m_cx
+                    );
+        
+                    let term_infeas = (m_by > eps_zero) && (
+                        norm(&d) * b_norm <= eps_infeas * m_by
+                    );
+        
+                    println!("{} {}", term_unbdd, term_infeas);
+        
+                    if term_unbdd {
+                        println!("unbounded");
+                        break;
+                    }
+        
+                    if term_infeas {
+                        println!("infeasible");
+                        break;
+                    }
+                }
+            }
 
             i += 1;
             if let Some(max_i) = max_iter {
                 if i >= max_i {
+                    println!("timeover");
                     break;
                 }
             }
         }
-        assert!(false);
+
+        let u_tau = x[n + m];
+        if u_tau > eps_zero {
+            scale(u_tau.recip(), &mut x);
+            println!("{:?}", x);
+        }
     }
 }
