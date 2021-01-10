49854e39c066547e9dd3f6ca5ee34fbc47a33af5
convexbrain <convexbrain@gmail.com>
2021-01-10 16:14:47 +0900

    updated: LinAlgEx Float

--- solver_rust_conic/src/linalgex.rs
+++ solver_rust_conic/src/linalgex.rs
@@ -6,9 +6,9 @@ use crate::solver::LinAlg;
 pub trait LinAlgEx<F: Float>: LinAlg<F> + Clone
 {
     // y = a*mat*x + b*y
-    fn transform_ge(transpose: bool, n_row: usize, n_col: usize, alpha: f64, mat: &[f64], x: &[f64], beta: f64, y: &mut[f64]);
+    fn transform_ge(transpose: bool, n_row: usize, n_col: usize, alpha: F, mat: &[F], x: &[F], beta: F, y: &mut[F]);
     // y = a*mat*x + b*y
-    fn transform_sp(n: usize, alpha: f64, mat: &[f64], x: &[f64], beta: f64, y: &mut[f64]);
+    fn transform_sp(n: usize, alpha: F, mat: &[F], x: &[F], beta: F, y: &mut[F]);
     fn proj_psd_worklen(sn: usize) -> usize;
-    fn proj_psd(x: &mut[f64], eps_zero: f64, work: &mut[f64]);
+    fn proj_psd(x: &mut[F], eps_zero: F, work: &mut[F]);
 }
