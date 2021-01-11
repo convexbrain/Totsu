e3362e1f490d4e1a682546eb589338d352950250
convexbrain <convexbrain@gmail.com>
2021-01-12 00:00:14 +0900

    updated: QCQP works

--- solver_rust_conic/src/probqcqp.rs
+++ solver_rust_conic/src/probqcqp.rs
@@ -108,11 +108,11 @@ where L: LinAlgEx<F>, F: Float
             // y_1 = 0*x_n + 0*x_1 + b*y_1
             L::scale(beta, y_1);
 
-            // y_o1 = a*2*vec_q^T*x_n + a*-1*x_1 + b*y_o1  (i = 0)
+            // y_o1 = a*2*vec_q^T*x_n + a*-2*x_1 + b*y_o1  (i = 0)
             //      = a*2*vec_q^T*x_n +    0*x_1 + b*y_o1  (i > 0)
             vec_q.trans_op(f2 * alpha, x_n, beta, y_o1);
             if i == 0 {
-                L::add(-alpha, x_1, y_o1)
+                L::add(-f2 * alpha, x_1, y_o1)
             }
 
             i += 1;
@@ -160,11 +160,11 @@ where L: LinAlgEx<F>, F: Float
             // y_1 = ... + 0*x_1 + ...
 
             // y_n = ... + a*2*vec_q*x_o1 + ...
-            // y_1 = ... + a*-1*x_o1 + ...  (i = 0)
+            // y_1 = ... + a*-2*x_o1 + ...  (i = 0)
             //       ... +    0*x_o1 + ...  (i > 0)
             vec_q.op(f2 * alpha, x_o1, f1, y_n);
             if i == 0 {
-                L::add(-alpha, x_o1, y_1);
+                L::add(-f2 * alpha, x_o1, y_1);
             }
 
             i += 1;
@@ -432,4 +432,49 @@ where L: LinAlgEx<F>, F: Float
 
         (op_c, op_a, op_b, cone, self.w_solver.as_mut())
     }
+}
+
+#[test]
+fn test_qcqp1()
+{
+    use float_eq::assert_float_eq;
+    use crate::stdlogger::PrintLogger;
+    use crate::f64lapack::F64LAPACK;
+    use crate::matop::MatType;
+    
+    type ASolver = Solver<F64LAPACK, f64>;
+    type AProbQCQP = ProbQCQP<F64LAPACK, f64>;
+    type AMatBuild = MatBuild<F64LAPACK, f64>;
+
+    let n = 2; // x0, x1
+    let m = 1;
+    let p = 0;
+    
+    // (1/2)(x - a)^2 + const
+    let mut syms_p = vec![AMatBuild::new(MatType::SymPack(n)); m + 1];
+    syms_p[0][(0, 0)] = 1.;
+    syms_p[0][(1, 1)] = 1.;
+
+    let mut vecs_q = vec![AMatBuild::new(MatType::General(n, 1)); m + 1];
+    vecs_q[0][(0, 0)] = -(5.); // -a0
+    vecs_q[0][(1, 0)] = -(4.); // -a1
+
+    // 1 - x0/b0 - x1/b1 <= 0
+    vecs_q[1][(0, 0)] = -1. / 2.; // -1/b0
+    vecs_q[1][(1, 0)] = -1. / 3.; // -1/b1
+
+    let mut scls_r = vec![0.; m + 1];
+    scls_r[1] = 1.;
+    
+    let mat_a = AMatBuild::new(MatType::General(p, n));
+
+    let vec_b = AMatBuild::new(MatType::General(p, 1));
+
+    let s = ASolver::new();
+    println!("{:?}", s.par);
+    let mut qp = AProbQCQP::new(syms_p, vecs_q, scls_r, mat_a, vec_b);
+    let rslt = s.solve(qp.problem(), PrintLogger).unwrap();
+    println!("{:?}", rslt);
+
+    assert_float_eq!(rslt.0[0..2], [5., 4.].as_ref(), abs_all <= 1e-3);
 }
