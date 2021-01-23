071ef8457531b650f56a986cab91ba69cd00ab12
convexbrain <convexbrain@gmail.com>
2021-01-23 15:52:16 +0900

    created: leastsq_qp

---
+++ experimental/leastsq_qp/src/main.rs
@@ -0,0 +1,83 @@
+use totsu::prelude::*;
+use totsu::operator::MatBuild;
+use totsu::linalg::F64LAPACK;
+use totsu::logger::PrintLogger;
+use totsu::problem::ProbQP;
+
+use rand::prelude::*;
+use rand_xoshiro::rand_core::SeedableRng;
+use rand_xoshiro::Xoshiro256StarStar;
+
+extern crate intel_mkl_src;
+
+type AMatBuild = MatBuild<F64LAPACK, f64>;
+type AProbQP = ProbQP<F64LAPACK, f64>;
+type ASolver = Solver<F64LAPACK, f64>;
+
+/// main
+fn main() -> std::io::Result<()> {
+
+    //----- make sample points for training
+
+    let mut rng = Xoshiro256StarStar::seed_from_u64(12);
+    let l = 2; // TODO 50; // # of dimension
+
+    let x = AMatBuild::new(MatType::General(l, 1))
+            .by_fn(|_, _| rng.gen_range(-2. ..= 2.)); // random l-dimensional vector
+    //println!("{}", x);
+
+    let mut y = vec![AMatBuild::new(MatType::General(l, 1)); l];
+    for yi in y.iter_mut() {
+        yi.set_by_fn(|_, _| rng.gen_range(-2. ..= 2.)); // random l-dimensional vector
+        //println!("{}", yi);
+    }
+
+    //----- formulate dual of hard-margin SVM as QP
+
+    let n = l;
+    let m = 0;
+    let p = 0;
+
+    let sym_p = AMatBuild::new(MatType::SymPack(n))
+                .by_fn(|r, c| {
+                    F64LAPACK::inner_prod(y[r].as_ref(), y[c].as_ref())
+                    //if r == c {1.} else {0.}
+                });
+    println!("{}", sym_p);
+
+    let mut vec_q = x.clone();
+    sym_p.op(-1., x.as_ref(), 0., vec_q.as_mut());
+    println!("{}", vec_q);
+
+    let mat_g = AMatBuild::new(MatType::General(m, n));
+    //println!("{}", mat_g);
+
+    let vec_h = AMatBuild::new(MatType::General(m, 1));
+    //println!("{}", vec_h);
+
+    let mat_a = AMatBuild::new(MatType::General(p, n));
+    //println!("{}", mat_a);
+
+    let vec_b = AMatBuild::new(MatType::General(p, 1));
+    //println!("{}", vec_b);
+
+    //----- solve QP
+
+    let mut s = ASolver::new();
+    //s.par.eps_acc = 1e-3;
+    s.par.log_period = 1000;
+    let mut qp = AProbQP::new(sym_p, vec_q, mat_g, vec_h, mat_a, vec_b);
+    let rslt = s.solve(qp.problem(), PrintLogger).unwrap();
+    println!("{:?}", rslt.0);
+
+    let mut max_absdiff = 0_f64;
+    for i in 0.. l {
+        let absdiff = (x[(i, 0)] - rslt.0[i]).abs();
+        max_absdiff = max_absdiff.max(absdiff);
+    }
+
+    println!("{:?}", x);
+    println!("{:.3e}", max_absdiff);
+
+    Ok(())
+}
