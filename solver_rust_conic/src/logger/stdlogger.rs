9b6f17a6b3db2245a1ae020320bd7681e45f617c
convexbrain <convexbrain@gmail.com>
2021-02-07 17:04:51 +0900

    updated: doctest IoWriteLogger

--- solver_rust_conic/src/logger/stdlogger.rs
+++ solver_rust_conic/src/logger/stdlogger.rs
@@ -13,6 +13,16 @@ impl core::fmt::Write for PrintLogger
 //
 
 /// Logger with a `std::io::Write` implementor.
+/// 
+/// Example of a logger writing to a new file.
+/// ```
+/// use totsu::logger::IoWriteLogger;
+/// use std::fs::File;
+/// use std::io::prelude::*;
+/// 
+/// let mut file = File::create("log.txt").unwrap();
+/// let logger = IoWriteLogger(&mut file);
+/// ```
 pub struct IoWriteLogger<'a, W: std::io::Write>(pub &'a mut W);
 
 impl<'a, W: std::io::Write> core::fmt::Write for IoWriteLogger<'a, W>
