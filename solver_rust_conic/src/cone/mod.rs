a24af27b5770f92a9050ffb5159377ac0f1d69cc
convexbrain <convexbrain@gmail.com>
2021-02-07 16:37:53 +0900

    updated: doc linalg

--- solver_rust_conic/src/cone/mod.rs
+++ solver_rust_conic/src/cone/mod.rs
@@ -12,9 +12,9 @@ pub trait Cone<F: Float>
     /// This is called by [`crate::solver::Solver::solve`] with passing dual variables as `x`.
     /// 
     /// Returns `Ok`, or `Err` if something fails.
-    /// * If `dual_cone` is true, project onto the dual cone \\(\mathcal{K}^*\\).
+    /// * If `dual_cone` is `true`, project onto the dual cone \\(\mathcal{K}^*\\).
     /// * `eps_zero` shall be the same value as [`crate::solver::SolverParam::eps_zero`].
-    /// * `x` is \\(x\\), a vector to be projected on entry, and shall be replaced with the projected vector on exit.
+    /// * `x` is \\(x\\), a vector to be projected before entry, and shall be replaced with the projected vector on exit.
     fn proj(&mut self, dual_cone: bool, eps_zero: F, x: &mut[F]) -> Result<(), ()>;
 
     /// Performs grouping for a diagonal preconditioning vector according to the cone \\(\mathcal{K}\\).
@@ -23,7 +23,7 @@ pub trait Cone<F: Float>
     /// split `dp_tau` into \\(n_1,\ldots,n_q\\) elements groups and call `group` for each.
     /// It is no need to call `group` for the group of \\(n_i=1\\).
     /// 
-    /// * `dp_tau` is a diagonal preconditioning vector to be grouped on entry,
+    /// * `dp_tau` is a diagonal preconditioning vector to be grouped before entry,
     ///   and shall be replaced with the grouped vector on exit.
     /// * `group` is a grouping function provided by [`crate::solver::Solver::solve`].
     fn product_group<G: Fn(&mut[F]) + Copy>(&self, dp_tau: &mut[F], group: G);
