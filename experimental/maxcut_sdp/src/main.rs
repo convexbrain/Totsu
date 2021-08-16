6093d3ed799241c7bd979e2bec7a3ef750e5d0f6
convexbrain <convexbrain@gmail.com>
2021-08-16 20:30:05 +0900

    updated: refactoring

--- experimental/maxcut_sdp/src/main.rs
+++ experimental/maxcut_sdp/src/main.rs
@@ -13,31 +13,35 @@ type AMatBuild = MatBuild<F64LAPACK, f64>;
 type AProbSDP = ProbSDP<F64LAPACK, f64>;
 type ASolver = Solver<F64LAPACK, f64>;
 
-/// main
-fn main() -> std::io::Result<()> {
-    env_logger::init();
+const EPS_ZERO: f64 = 1e-3;
+
+fn make_adj_matrix(x_num: usize, y_num: usize, seed: u64) -> AMatBuild
+{
+    let l = x_num * y_num; // # of nodes
+    let mut rng = Xoshiro256StarStar::seed_from_u64(seed);
 
     //----- make adjacent matrix
 
-    let l = 9; // # of nodes
     let mut sym_w = AMatBuild::new(MatType::SymPack(l));
-    sym_w[(0, 3)] = 1.;
-    sym_w[(1, 2)] = 1.;
-    sym_w[(2, 5)] = 1.;
-    sym_w[(3, 4)] = 1.;
-    sym_w[(4, 5)] = 1.;
-    sym_w[(4, 7)] = 1.;
-    sym_w[(5, 8)] = 1.;
-    sym_w[(6, 7)] = 1.;
-    /*
-    let l = 4; // # of nodes
-    let mut sym_w = AMatBuild::new(MatType::SymPack(l));
-    sym_w[(0, 1)] = 1.;
-    sym_w[(0, 2)] = 1.;
-    sym_w[(0, 3)] = 1.;
-    */
+    for i in 0..l {
+        let x = i / y_num;
+        let y = i % y_num;
 
-    //----- formulate max-cut as SDP
+        if x < x_num - 1 {
+            sym_w[(i, i + y_num)] = rng.sample(StandardNormal); // positive: dissimilar, negative: similar
+        }
+        if y < y_num - 1 {
+            sym_w[(i, i + 1)] = rng.sample(StandardNormal); // positive: dissimilar, negative: similar
+        }
+    }
+    //println!("{:?}", sym_w);
+
+    sym_w
+}
+
+fn make_sdp(sym_w: &AMatBuild) -> AProbSDP
+{
+    let l = sym_w.size().0; // # of nodes
 
     let vec_c = sym_w.clone().reshape_colvec();
     //println!("{}", vec_c);
@@ -67,22 +71,21 @@ fn main() -> std::io::Result<()> {
                 .by_fn(|_, _| 1.);
     //println!("{}", vec_b);
 
-    //----- solve SDP
+    AProbSDP::new(vec_c, syms_f, mat_a, vec_b, EPS_ZERO)
+}
 
-    let s = ASolver::new().par(|p| {
-        p.eps_acc = 1e-3;
-    });
-    let mut sdp = AProbSDP::new(vec_c, syms_f, mat_a, vec_b, s.par.eps_zero);
-    let rslt = s.solve(sdp.problem()).unwrap();
-    //println!("{:?}", rslt);
+fn sample_feasible(rslt_x: &[f64], sym_w: &AMatBuild, seed: u64) -> (f64, AMatBuild)
+{
+    let l = sym_w.size().0; // # of nodes
+    let mut rng = Xoshiro256StarStar::seed_from_u64(seed);
 
-    //----- random sampling of feasible point
+    //----- random sampling to find the best feasible point
 
     let mut sym_x = AMatBuild::new(MatType::SymPack(l));
     let mut kk = 0;
     for j in 0..l {
         for i in 0..=j {
-            sym_x[(i, j)] = rslt.0[kk];
+            sym_x[(i, j)] = rslt_x[kk];
             kk += 1;
         }
     }
@@ -90,20 +93,70 @@ fn main() -> std::io::Result<()> {
     sym_x.set_sqrt(1e-3);
     //println!("{:?}", sym_x);
 
-    let mut rng = Xoshiro256StarStar::seed_from_u64(10000);
+    let mut tmpx = AMatBuild::new(MatType::General(l, 1));
+    let mut x = AMatBuild::new(MatType::General(l, 1));
+    let mut o = [0f64; 1];
+    let mut o_feas = None;
+    let mut x_feas = AMatBuild::new(MatType::General(l, 1));
 
-    let mut nx = vec![0_f64; l];
-    let mut x = vec![0_f64; l];
-    for e in &mut nx {
-        *e = rng.sample(StandardNormal);
-    }
-    //println!("{:?}", nx);
+    for _ in 0..l {
+        tmpx.set_by_fn(|_, _| rng.sample(StandardNormal));
+        //println!("{:?}", tmpx);
+    
+        sym_x.op(1., tmpx.as_ref(), 0., x.as_mut());
+        for e in x.as_mut() {
+            *e = if *e > 0. {1.} else {-1.};
+        }
+        //println!("{:?}", x);
 
-    sym_x.op(1., &nx, 0., &mut x);
-    for e in &mut x {
-        *e = if *e > 0. {1.} else {-1.};
+        sym_w.op(1., x.as_ref(), 0., tmpx.as_mut());
+        tmpx.trans_op(1., x.as_ref(), 0., &mut o);
+        //println!("{}", o[0]);
+
+        if let Some(o_feas_some) = o_feas {
+            if o_feas_some > o[0] {
+                o_feas = Some(o[0]);
+                x_feas.set_iter_colmaj(x.as_ref());
+            }
+        }
+        else {
+            o_feas = Some(o[0]);
+            x_feas.set_iter_colmaj(x.as_ref());
+        }
     }
-    println!("{:?}", x);
+    let o_feas = o_feas.unwrap();
+    //println!("{}", o_feas);
+    //println!("{:?}", x_feas);    
+
+    (o_feas, x_feas)
+}
+
+/// main
+fn main() -> std::io::Result<()> {
+    env_logger::init();
+
+    //----- make adjacent matrix
+
+    let x_num = 4;
+    let y_num = 4;
+    let sym_w = make_adj_matrix(x_num, y_num, 10000);
+
+    //----- formulate max-cut as SDP
+
+    let mut sdp = make_sdp(&sym_w);
+
+    //----- solve SDP
+
+    let s = ASolver::new().par(|p| {
+        p.eps_acc = EPS_ZERO;
+    });
+    let rslt = s.solve(sdp.problem()).unwrap();
+    //println!("{:?}", rslt);
+
+    //----- random sampling to find the best feasible point
+
+    let (_, x_feas) = sample_feasible(&rslt.0, &sym_w, 20000);
+    println!("{:?}", x_feas);    
 
     Ok(())
 }
