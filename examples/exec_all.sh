46f2e4199f53e5732cd3808545c74969f53aac16
convexbrain <convexbrain@gmail.com>
2022-08-14 15:24:46 +0900

    updated: dependencies ver. up

--- examples/exec_all.sh
+++ examples/exec_all.sh
@@ -1,12 +1,12 @@
 
-(cd l1reg_lp         && RUST_LOG=debug cargo run --release) || exit
-(cd svm_qp           && RUST_LOG=debug cargo run --release) || exit
-(cd trajplan_qcqp    && RUST_LOG=debug cargo run --release) || exit
-(cd toruscompl_socp  && RUST_LOG=debug cargo run --release) || exit
-(cd partitioning_sdp && RUST_LOG=debug cargo run --release) || exit
-(cd imgnr_udef       && RUST_LOG=debug cargo run --release) || exit
+(cd l1reg_lp         && pwd && RUST_LOG=debug cargo run --release) || exit
+(cd svm_qp           && pwd && RUST_LOG=debug cargo run --release) || exit
+(cd trajplan_qcqp    && pwd && RUST_LOG=debug cargo run --release) || exit
+(cd toruscompl_socp  && pwd && RUST_LOG=debug cargo run --release) || exit
+(cd partitioning_sdp && pwd && RUST_LOG=debug cargo run --release) || exit
+(cd imgnr_udef       && pwd && RUST_LOG=debug cargo run --release) || exit
 
-(cd nostd_cortex-m && cargo build --release && sh run_qemu.sh target/thumbv7m-none-eabi/release/nostd_cortex-m) || exit
+(cd nostd_cortex-m && pwd && cargo build --release && sh run_qemu.sh target/thumbv7m-none-eabi/release/nostd_cortex-m) || exit
 
 echo
 echo "=================="
