039024b1a508214e5760163d205e4d6ac86d4b00
convexbrain <convexbrain@gmail.com>
2022-09-10 01:16:46 +0900

    updated: use thread_local in cuda_mgr

--- solver_rust_conic/src/linalg/f32cuda.rs
+++ solver_rust_conic/src/linalg/f32cuda.rs
@@ -15,15 +15,16 @@ use once_cell::sync::Lazy;
 
 pub mod cuda_mgr
 {
+    use std::thread_local;
+    use std::rc::Rc;
     use rustacuda::prelude::*;
     use rustacuda::memory::DeviceBuffer;
     use cublas_sys::*;
     use super::cusolver_sys_partial::*;
-    use once_cell::sync::Lazy;
 
     struct CudaManager
     {
-        cuda_ctx: Context,
+        cuda_ctx: Rc<Context>,
         cublas_handle: cublasHandle_t,
         cusolver_handle: cusolverDnHandle_t,
     }
@@ -31,133 +32,120 @@ pub mod cuda_mgr
     unsafe impl Send for CudaManager {}
     unsafe impl Sync for CudaManager {}
 
-    // TODO: try thread_local
-    static CUDA_MANAGER: Lazy<CudaManager> = Lazy::new(|| {
-        // Initialize the CUDA API
-        let r = rustacuda::init(CudaFlags::empty());
-        if r.is_err() {
-            log::error!("CUDA driver initialization failed");
-        }
-        r.unwrap();
-
-        // API version
-        log::info!(
-            "CUDA driver API version: {}.{}",
-            rustacuda::CudaApiVersion::get().unwrap().major(),
-            rustacuda::CudaApiVersion::get().unwrap().minor(),
-        );
-
-        // Get the first device
-        // TODO: num_devices
-        let device = Device::get_device(0);
-        if device.is_err() {
-            log::error!("CUDA device not found");
-        }
-        let device = device.unwrap();
-
-        // Device name
-        log::info!("CUDA device name: {}", device.name().unwrap());
-
-        // Create a context associated to this device
-        let cuda_ctx = Context::create_and_push(
-            ContextFlags::MAP_HOST | ContextFlags::SCHED_AUTO,
-            device
-        );
-        if cuda_ctx.is_err() {
-            log::error!("CUDA context failed to create");
-        }
-        let cuda_ctx = cuda_ctx.unwrap();
-
-        // cuBLAS handle
-        let mut cublas_handle: cublasHandle_t = core::ptr::null_mut();
-        unsafe {
-            let st = cublasCreate_v2(&mut cublas_handle);
-            if st != cublasStatus_t::CUBLAS_STATUS_SUCCESS {
-                log::error!("cuBLAS handle failed to create");
-            }
-            assert_eq!(st, cublasStatus_t::CUBLAS_STATUS_SUCCESS);
-        }
-
-        // cuSOLVER handle
-        let mut cusolver_handle: cusolverDnHandle_t = core::ptr::null_mut();
-        unsafe {
-            let st = cusolverDnCreate(&mut cusolver_handle);
-            if st != cusolverStatus_t::CUSOLVER_STATUS_SUCCESS {
-                log::error!("cuSOLVER handle failed to create");
-            }
-            assert_eq!(st, cusolverStatus_t::CUSOLVER_STATUS_SUCCESS);
-        }
-
-        log::debug!("CUDA_MANAGER created");
-        CudaManager {
-            cuda_ctx,
-            cublas_handle,
-            cusolver_handle,
-        }
-    });
-
     impl CudaManager
     {
-        fn buf_from_slice(&self, s: &[f32]) -> DeviceBuffer<f32>
+        fn new() -> CudaManager
         {
-            DeviceBuffer::from_slice(s).unwrap()
-        }
+            // Initialize the CUDA API
+            let r = rustacuda::init(CudaFlags::empty());
+            if r.is_err() {
+                log::error!("CUDA driver initialization failed");
+            }
+            r.unwrap();
 
-        fn buf_zeroes<T>(&self, length: usize) -> DeviceBuffer<T>
-        {
+            // API version
+            log::info!(
+                "CUDA driver API version: {}.{}",
+                rustacuda::CudaApiVersion::get().unwrap().major(),
+                rustacuda::CudaApiVersion::get().unwrap().minor(),
+            );
+
+            // Get the first device
+            // TODO: num_devices
+            let device = Device::get_device(0);
+            if device.is_err() {
+                log::error!("CUDA device not found");
+            }
+            let device = device.unwrap();
+
+            // Device name
+            log::info!("CUDA device name: {}", device.name().unwrap());
+
+            // Create a context associated to this device
+            let cuda_ctx = Context::create_and_push(
+                ContextFlags::MAP_HOST | ContextFlags::SCHED_AUTO,
+                device
+            );
+            if cuda_ctx.is_err() {
+                log::error!("CUDA context failed to create");
+            }
+            let cuda_ctx = Rc::new(cuda_ctx.unwrap());
+
+            // cuBLAS handle
+            let mut cublas_handle: cublasHandle_t = core::ptr::null_mut();
             unsafe {
-                DeviceBuffer::zeroed(length).unwrap()
+                let st = cublasCreate_v2(&mut cublas_handle);
+                if st != cublasStatus_t::CUBLAS_STATUS_SUCCESS {
+                    log::error!("cuBLAS handle failed to create");
+                }
+                assert_eq!(st, cublasStatus_t::CUBLAS_STATUS_SUCCESS);
+            }
+
+            // cuSOLVER handle
+            let mut cusolver_handle: cusolverDnHandle_t = core::ptr::null_mut();
+            unsafe {
+                let st = cusolverDnCreate(&mut cusolver_handle);
+                if st != cusolverStatus_t::CUSOLVER_STATUS_SUCCESS {
+                    log::error!("cuSOLVER handle failed to create");
+                }
+                assert_eq!(st, cusolverStatus_t::CUSOLVER_STATUS_SUCCESS);
+            }
+
+            log::debug!("CUDA_MANAGER created");
+            CudaManager {
+                cuda_ctx,
+                cublas_handle,
+                cusolver_handle,
             }
         }
-
-        fn context(&self) -> &Context
-        {
-            &self.cuda_ctx
-        }
-
-        fn cublas_handle(&self) -> cublasHandle_t
-        {
-            self.cublas_handle
-        }
-
-        fn cusolver_handle(&self) -> cusolverDnHandle_t
-        {
-            self.cusolver_handle
-        }
     }
+
+    thread_local!(static CUDA_MANAGER: CudaManager = CudaManager::new());
 
     /// TODO: doc
     pub fn buf_from_slice(s: &[f32]) -> DeviceBuffer<f32>
     {
-        CUDA_MANAGER.buf_from_slice(s)
+        CUDA_MANAGER.with(|_| { // ensure that rustacuda::init is done
+            DeviceBuffer::from_slice(s).unwrap()
+        })
     }
 
     /// TODO: doc
     pub fn buf_zeroes<T>(length: usize) -> DeviceBuffer<T>
     {
-        CUDA_MANAGER.buf_zeroes(length)
+        CUDA_MANAGER.with(|_| { // ensure that rustacuda::init is done
+            unsafe {
+                DeviceBuffer::zeroed(length).unwrap()
+            }
+        })
     }
 
     /// TODO: doc
-    pub fn context() -> &'static Context
+    pub fn context() -> Rc<Context>
     {
-        CUDA_MANAGER.context()
+        CUDA_MANAGER.with(|mgr| {
+            mgr.cuda_ctx.clone()
+        })
     }
 
     /// TODO: doc
     pub fn cublas_handle() -> cublasHandle_t
     {
-        CUDA_MANAGER.cublas_handle()
+        CUDA_MANAGER.with(|mgr| {
+            mgr.cublas_handle
+        })
     }
 
     /// TODO: doc
     pub fn cusolver_handle() -> cusolverDnHandle_t
     {
-        CUDA_MANAGER.cusolver_handle()
+        CUDA_MANAGER.with(|mgr| {
+            mgr.cusolver_handle
+        })
     }
 }
 
-
+//
 
 #[derive(Eq, PartialEq, Copy, Clone)]
 enum CUDASliceMut
