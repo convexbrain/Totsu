4c2f412bff8080abf46288b7491b9881a4197c94
convexbrain <convexbrain@gmail.com>
2022-09-09 23:14:01 +0900

    updated: sqrt_spmat by cusolver, tidyup

--- solver_rust_conic/src/linalg/f32cuda.rs
+++ solver_rust_conic/src/linalg/f32cuda.rs
@@ -1,4 +1,3 @@
-use core::ops::{Index, IndexMut};
 use std::vec::Vec;
 use std::vec;
 use std::collections::HashMap;
@@ -6,7 +5,6 @@ use std::sync::{Mutex, Arc};
 use std::boxed::Box;
 use std::pin::Pin;
 use super::{SliceRef, SliceMut, SliceLike, LinAlg, LinAlgEx};
-use crate::utils::SplitN;
 use rustacuda::prelude::*;
 use rustacuda::memory::{DeviceBuffer, DeviceSlice};
 use cublas_sys::*;
@@ -59,6 +57,7 @@ impl CudaManager
     }
 }
 
+// TODO: try thread_local
 /// TODO: doc
 pub static CUDA_MANAGER: Lazy<CudaManager> = Lazy::new(|| {
     // Initialize the CUDA API
@@ -298,6 +297,7 @@ impl Drop for SliceManager
     }
 }
 
+// TODO: try thread_local
 // TODO: encapsulate try_lock
 static SLICE_MANAGER: Lazy<Mutex<SliceManager>> = Lazy::new(|| {
     Mutex::new(SliceManager {
@@ -417,6 +417,15 @@ impl SliceLike for F32CUDASlice
 
 impl F32CUDASlice
 {
+    /// TODO: doc
+    pub fn new_zeroes(length: usize) -> SliceMut<'static, F32CUDASlice>
+    {
+        let mut mgr = SLICE_MANAGER.try_lock().unwrap();
+        let cs = mgr.new_zeroes(length);
+
+        SliceMut {s: cs}
+    }    
+
     /// TODO: doc
     pub fn get_dev(&self) -> &DeviceSlice<f32>
     {
@@ -591,286 +600,6 @@ impl LinAlg for F32CUDA
 
 //
 
-struct MatIdx<'a>
-{
-    n_row: usize,
-    n_col: usize,
-    mat: &'a[f32],
-    transpose: bool,
-}
-
-impl<'a> MatIdx<'a>
-{
-    fn idx(&self, (r, c): (usize, usize)) -> usize
-    {
-        let (r, c) = if !self.transpose {(r, c)} else {(c, r)};
-        
-        assert!(r < self.n_row);
-        assert!(c < self.n_col);
-
-        c * self.n_row + r
-    }
-}
-
-impl<'a> Index<(usize, usize)> for MatIdx<'a>
-{
-    type Output = f32;
-
-    fn index(&self, index: (usize, usize)) -> &Self::Output
-    {
-        &self.mat[self.idx(index)]
-    }
-}
-
-//
-
-struct MatIdxMut<'a>
-{
-    n_row: usize,
-    n_col: usize,
-    mat: &'a mut[f32],
-    transpose: bool,
-}
-
-impl<'a> MatIdxMut<'a>
-{
-    fn col_vec(&self, c: usize) -> &[f32]
-    {
-        assert!(c < self.n_col);
-        assert!(!self.transpose);
-
-        let (_, v) = self.mat.split_at(c * self.n_row);
-        let (v, _) = v.split_at(self.n_row);
-
-        v
-    }
-
-    fn clear(&mut self)
-    {
-        for a in self.mat.iter_mut() {
-            *a = 0.;
-        }
-    }
-}
-
-impl<'a> Index<(usize, usize)> for MatIdxMut<'a>
-{
-    type Output = f32;
-
-    fn index(&self, index: (usize, usize)) -> &Self::Output
-    {
-        let mat_idx = MatIdx {
-            n_row: self.n_row,
-            n_col: self.n_col,
-            mat: self.mat,
-            transpose: self.transpose,
-        };
-        
-        &self.mat[mat_idx.idx(index)]
-    }
-}
-
-impl<'a> IndexMut<(usize, usize)> for MatIdxMut<'a>
-{
-    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output
-    {
-        let mat_idx = MatIdx {
-            n_row: self.n_row,
-            n_col: self.n_col,
-            mat: self.mat,
-            transpose: self.transpose,
-        };
-        
-        &mut self.mat[mat_idx.idx(index)]
-    }
-}
-
-//
-
-struct SpMatIdx<'a>
-{
-    n: usize,
-    mat: &'a[f32],
-}
-
-impl<'a> SpMatIdx<'a>
-{
-    fn idx(&self, (r, c): (usize, usize)) -> usize
-    {
-        assert!(r < self.n);
-        assert!(c < self.n);
-
-        let (r, c) = if r < c {(r, c)} else {(c, r)};
-
-        c * (c + 1) / 2 + r
-    }
-}
-
-impl<'a> Index<(usize, usize)> for SpMatIdx<'a>
-{
-    type Output = f32;
-
-    fn index(&self, index: (usize, usize)) -> &Self::Output
-    {
-        &self.mat[self.idx(index)]
-    }
-}
-
-//
-
-struct SpMatIdxMut<'a>
-{
-    n: usize,
-    mat: &'a mut[f32],
-}
-
-impl<'a> SpMatIdxMut<'a>
-{
-    fn clear(&mut self)
-    {
-        for a in self.mat.iter_mut() {
-            *a = 0.;
-        }
-    }
-
-    fn rank1op(&mut self, alpha: f32, x: &[f32])
-    {
-        assert_eq!(x.len(), self.n);
-
-        for c in 0.. self.n {
-            for r in 0..= c {
-                self[(r, c)] = alpha * x[r] * x[c] + self[(r, c)];
-            }
-        }
-    }
-}
-
-impl<'a> Index<(usize, usize)> for SpMatIdxMut<'a>
-{
-    type Output = f32;
-
-    fn index(&self, index: (usize, usize)) -> &Self::Output
-    {
-        let sp_mat_idx = SpMatIdx {
-            n: self.n,
-            mat: self.mat,
-        };
-        
-        &self.mat[sp_mat_idx.idx(index)]
-    }
-}
-
-impl<'a> IndexMut<(usize, usize)> for SpMatIdxMut<'a>
-{
-    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output
-    {
-        let sp_mat_idx = SpMatIdx {
-            n: self.n,
-            mat: self.mat,
-        };
-        
-        &mut self.mat[sp_mat_idx.idx(index)]
-    }
-}
-
-//
-
-fn jacobi_eig(spmat_x: &mut SpMatIdxMut, mat_z: &mut MatIdxMut, eps: f32)
-{
-    let n = spmat_x.n;
-    let tol = eps * eps;
-    let f0 = 0.;
-    let f1 = 1.;
-    let f2 = f1 + f1;
-
-    let mut conv = false;
-
-    while !conv {
-        conv = true;
-
-        for i in 0.. n {
-            for j in i + 1.. n {
-                let a = spmat_x[(i, i)];
-                let b = spmat_x[(j, j)];
-                let d = spmat_x[(i, j)];
-
-                if (d * d > tol * a * b) && (d * d > tol) {
-                    conv = false;
-
-                    let zeta = (b - a) / (f2 * d);
-                    let t = if zeta > f0 {
-                        f1 / (zeta + (f1 + zeta * zeta).sqrt())
-                    }
-                    else {
-                        -f1 / (-zeta + (f1 + zeta * zeta).sqrt())
-                    };
-                    let c = (f1 + t * t).sqrt().recip();
-                    let s = c * t;
-
-                    for k in 0.. n {
-                        let xi = spmat_x[(k, i)];
-                        let xj = spmat_x[(k, j)];
-                        spmat_x[(k, i)] = c * xi - s * xj;
-                        spmat_x[(k, j)] = s * xi + c * xj;
-
-                        let zi = mat_z[(k, i)];
-                        let zj = mat_z[(k, j)];
-                        mat_z[(k, i)] = c * zi - s * zj;
-                        mat_z[(k, j)] = s * zi + c * zj;
-                    }
-
-                    spmat_x[(i, i)] = c * c * a + s * s * b - f2 * c * s * d;
-                    spmat_x[(j, j)] = s * s * a + c * c * b + f2 * c * s * d;
-                    spmat_x[(i, j)] = f0;
-                }
-            }
-        }
-    }
-}
-
-fn eig_func<E>(spmat_x: &mut SpMatIdxMut, eps_zero: f32, work: &mut[f32], func: E)
-where E: Fn(f32)->Option<f32>
-{
-    let f1 = 1.;
-
-    let n = spmat_x.n;
-
-    let (w, z) = work.split2(n, n * n).unwrap();
-
-    let mut mat_z = MatIdxMut {
-        n_row: n, n_col: n, mat: z, transpose: false,
-    };
-
-    mat_z.clear();
-    for i in 0.. n {
-        mat_z[(i, i)] = f1;
-    }
-
-    jacobi_eig(spmat_x, &mut mat_z, eps_zero);
-
-    for i in 0.. n {
-        w[i] = spmat_x[(i, i)];
-    }
-
-    spmat_x.clear();
-    for i in 0.. n {
-        if let Some(e) = func(w[i]) {
-            let zcol = mat_z.col_vec(i);
-            spmat_x.rank1op(e, zcol);
-        }
-    }
-}
-
-fn eig_func_worklen(n: usize) -> usize
-{
-    let len_w = n;
-    let len_z = n * n;
-
-    len_w + len_z
-}
-
-//
-
 impl LinAlgEx for F32CUDA
 {
     // y = a*mat*x + b*y
@@ -960,11 +689,11 @@ impl LinAlgEx for F32CUDA
         assert_eq!(n * (n + 1) / 2, sn);
         assert!(work.len() >= Self::proj_psd_worklen(sn));
 
-        let a = SLICE_MANAGER.try_lock().unwrap().new_zeroes(n * n);
+        let mut a = F32CUDASlice::new_zeroes(n * n);
 
-        vec_to_mat(x, a, true);
+        vec_to_mat(x, &mut a, true);
     
-        eig_func2(a, n, |e| {
+        eig_func(&mut a, n, |e| {
             if e > 0. {
                 Some(e)
             }
@@ -973,18 +702,19 @@ impl LinAlgEx for F32CUDA
             }
         });
 
-        mat_to_vec(a, x, true);
+        mat_to_vec(&mut a, x, true);
     }
 
     fn sqrt_spmat_worklen(n: usize) -> usize
     {
-        // TODO: cusolver
+        // TODO: work memory
         eig_func_worklen(n)
     }
 
-    fn sqrt_spmat(mat: &mut F32CUDASlice, eps_zero: f32, work: &mut F32CUDASlice)
+    fn sqrt_spmat(mat: &mut F32CUDASlice, _eps_zero: f32, work: &mut F32CUDASlice)
     {
-        // TODO: cusolver
+        // TODO: cuda test
+        /*
         let mat = mat.get_mut();
         let work = work.get_mut();
 
@@ -1007,17 +737,48 @@ impl LinAlgEx for F32CUDA
                 None
             }
         });
+        */
+        let sn = mat.len();
+
+        let n = (((8 * sn + 1) as f64).sqrt() as usize - 1) / 2;
+        assert_eq!(n * (n + 1) / 2, sn);
+        assert!(work.len() >= Self::proj_psd_worklen(sn));
+
+        let mut a = F32CUDASlice::new_zeroes(n * n);
+
+        vec_to_mat(mat, &mut a, false);
+    
+        eig_func(&mut a, n, |e| {
+            if e > 0. {
+                Some(e.sqrt())
+            }
+            else {
+                None
+            }
+        });
+
+        mat_to_vec(&mut a, mat, false);
     }
 }
 
-// TODO: remove 2
-fn eig_func2<E>(a: &mut F32CUDASlice, n: usize, func: E)
+//
+
+// TODO: work memory
+fn eig_func_worklen(n: usize) -> usize
+{
+    let len_w = n;
+    let len_z = n * n;
+
+    len_w + len_z
+}
+
+fn eig_func<E>(a: &mut F32CUDASlice, n: usize, func: E)
 where E: Fn(f32)->Option<f32>
 {
     // TODO: work memory
     let mut meig: i32 = 0;
     let mut lwork: i32 = 0;
-    let w = SLICE_MANAGER.try_lock().unwrap().new_zeroes(n);
+    let mut w = F32CUDASlice::new_zeroes(n);
 
     unsafe {
         let st = cusolverDnSsyevdx_bufferSize(
