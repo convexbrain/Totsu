afdb391fe7b36c376fd7b9fc4668054275d902c9
convexbrain <convexbrain@gmail.com>
2022-09-12 23:46:09 +0900

    updated: qcqp restored

--- solver_rust_conic/src/problem/mod.rs
+++ solver_rust_conic/src/problem/mod.rs
@@ -4,16 +4,17 @@
 
 mod lp;   // std, Float
 mod qp;   // std, Float
-/* TODO: restore
 mod qcqp; // std, Float
+
+/* TODO: restore
 mod socp; // std, Float
 mod sdp;  // std, Float
  */
 
 pub use lp::ProbLP;
 pub use qp::ProbQP;
-/* TODO: restore
 pub use qcqp::ProbQCQP;
+/* TODO: restore
 pub use socp::ProbSOCP;
 pub use sdp::ProbSDP;
  */
